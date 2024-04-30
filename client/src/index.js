876fc1d7799c4cec993a16e4e896a7fa59192a74
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-04-30 17:38:37 -0400

    Added Store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { store } from './store/store'; // Import Redux store
+import { Provider } from 'react-redux'; // Import Provider component from react-redux
+
+// Render the App component wrapped in the Provider component, passing the Redux store
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root') // Mounting point in the HTML document
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ client/src/store/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  transaction: []
+};
+
+export const expenseSlice = createSlice({
+  name: 'expense',
+  initialState,
+  reducers: {
+    getTransactions: (state) => {
+      // Your reducer logic goes here
+    } 
+  }
+});
+
+export const { getTransactions } = expenseSlice.actions;
+export default expenseSlice.reducer;
